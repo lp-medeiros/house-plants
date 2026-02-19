409d7d9c4050b9ecba49ff89564b7f70f6009853
lp-medeiros <luis.medeiros.dp@gmail.com>
2026-02-19 12:47:26 -0300

    correção de responsividade do text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ function About(){
         <div id='sobre'>
         <div className="about-container">
             <div className="about-text">
-                <h1>Sou Fernando! <span className="highlight">Fundador</span> da FMP paisagismo.</h1>
+                <h1>Sou Fernando! <span className="highlight">Fundador</span> da FMP Paisagismo.</h1>
                 <p>A FMP Paisagismo nasceu da paixão pela <span className="highlight">natureza</span> e do desejo de transformar espaços em lugares que tocam a alma.</p>
                 <p>Criamos projetos de paisagismo e jardinagem com um cuidado que vai além do visual: ouvimos cada cliente, entendemos seus sonhos e os traduzimos em espaços vivos, equilibrados e cheios de significado. Para nós, um jardim bem cuidado é saúde, é bem-estar, é qualidade de vida.</p>
                 <p>Respeitamos profundamente a natureza em tudo que fazemos priorizando espécies adequadas, técnicas sustentáveis e o equilíbrio entre o que o homem projeta e o que a natureza já criou com perfeição.</p>
