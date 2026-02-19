78bc32135c6d6859c1c7733e6248c2cfd006b610
lp-medeiros <luis.medeiros.dp@gmail.com>
2026-02-19 12:35:31 -0300

    correção de import

--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -5,20 +5,13 @@ import p3 from '../images/project 3.jpg'
 import p4 from '../images/project 4.jpg'
 import p5 from '../images/project 5.jpg'
 import p6 from '../images/project 6.jpg'
-import Lightbox from 'yet-another-react-lightbox'
-import 'yet-another-react-lightbox/styles.css'
-import { useState } from 'react'
-
-
 function Portifolio(){
-    const [index, setIndex] = useState(-1);
-    const slides = [{src: p1}, {src: p2}, {src: p3}, {src: p4}, {src: p5}, {src: p6}]
     return(
         <div id='portifolio'>
         <div className='portifolio-container'>
             <h2 className='title-portifolio'>Conheça Nosso Portifólio</h2>
             <div className='grid'>
-                <img src={p1} alt='Project 1' onClick={() => setIndex(1)} style={{cursor:'pointer'}}/>
+                <img src={p1} alt='Project 1'/>
                 <img src={p2} alt='Project 2'/>
                 <img src={p3} alt='Project 3'/>
                 <img src={p4} alt='Project 4'/>
